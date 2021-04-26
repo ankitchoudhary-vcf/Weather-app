36b753a79adb0e3155e0ad2face9fab7b85a6bef
ankit <ac2000choudhary@gmail.com>
2021-04-26 10:21:19 +0530

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weather app</title>
 
-    <link rel="stylesheet" href="../css/main.css">
+    <!-- <link rel="stylesheet" href="../css/main.css"> -->
     <script src="https://rawgithub.com/darkskyapp/skycons/master/skycons.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/places.js@1.18.1" defer></script>
     <script src="../js/app.js" defer></script>
 </head>
 <body class="bg-blue-200">
     <div id="app" class="flex justify-center pt-16">
-        <weather-app></weather-app>
+        <weatherApp-component></weatherApp-component>
     </div>
 </body>
 </html>
